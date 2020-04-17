12048d23d6643a79e718cc4e1872174563957b17
wuyichen24 <wuyichen24@gmail.com>
2020-04-16 18:13:19 -0600

    add diagrams

--- ftgo-kitchen-service/src/main/java/com/ftgo/kitchenservice/domain/TicketState.java
+++ ftgo-kitchen-service/src/main/java/com/ftgo/kitchenservice/domain/TicketState.java
@@ -1,5 +1,13 @@
 package com.ftgo.kitchenservice.domain;
 
+/**
+ * The enum type for the states of tickets.
+ * 
+ * @author  Wuyi Chen
+ * @date    04/16/2019
+ * @version 1.0
+ * @since   1.0
+ */
 public enum TicketState {
 	CREATE_PENDING, 
 	AWAITING_ACCEPTANCE, 
--- ftgo-order-service-api/src/main/java/com/ftgo/orderservice/api/model/OrderState.java
+++ ftgo-order-service-api/src/main/java/com/ftgo/orderservice/api/model/OrderState.java
@@ -1,10 +1,18 @@
 package com.ftgo.orderservice.api.model;
 
+/**
+ * The enum type for the states of tickets.
+ * 
+ * @author  Wuyi Chen
+ * @date    04/16/2019
+ * @version 1.0
+ * @since   1.0
+ */
 public enum OrderState {
-  APPROVAL_PENDING,
-  APPROVED,
-  REJECTED,
-  CANCEL_PENDING,
-  CANCELLED,
-  REVISION_PENDING,
+	APPROVAL_PENDING,
+	APPROVED,
+	REJECTED,
+	CANCEL_PENDING,
+	CANCELLED,
+	REVISION_PENDING,
 }
