01ca741557edcf2f538408b5943b66f41a239e05
wuyichen24 <wuyichen24@gmail.com>
2020-04-25 20:39:50 -0600

    make consumer service runnable

--- ftgo-consumer-service/src/main/java/com/ftgo/consumerservice/ConsumerServiceApplication.java
+++ ftgo-consumer-service/src/main/java/com/ftgo/consumerservice/ConsumerServiceApplication.java
@@ -2,9 +2,13 @@ package com.ftgo.consumerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.ftgo.consumerservice.domain.ConsumerWebConfiguration;
+import com.ftgo.consumerservice.model.Consumer;
+import com.ftgo.consumerservice.repository.ConsumerRepository;
 import com.ftgo.eventstore.examples.customersandorders.commonswagger.CommonSwaggerConfiguration;
 
 import io.eventuate.tram.jdbckafka.TramJdbcKafkaConfiguration;
@@ -19,6 +23,8 @@ import io.eventuate.tram.jdbckafka.TramJdbcKafkaConfiguration;
  */
 @SpringBootApplication
 @Import({ ConsumerWebConfiguration.class, TramJdbcKafkaConfiguration.class, CommonSwaggerConfiguration.class })
+@EntityScan(basePackageClasses = {Consumer.class})
+@EnableJpaRepositories(basePackageClasses = {ConsumerRepository.class})
 public class ConsumerServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumerServiceApplication.class, args);
